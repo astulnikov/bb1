a23cd9c83c12c11c7943f944dfd3cfc225730943
aliaksei.stulnikau <aliaksei.stulnikau@revolut.com>
2021-12-05 14:44:17 +0100

    Tune commands

--- app/src/main/java/com/astulnikov/bb1mainunit/communication/command/TurnToAngleCommand.kt
+++ app/src/main/java/com/astulnikov/bb1mainunit/communication/command/TurnToAngleCommand.kt
@@ -1,13 +1,14 @@
 package com.astulnikov.bb1mainunit.communication.command
 
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 /**
  * @author aliaksei.stulnikau 14.02.18.
  */
 class TurnToAngleCommand(val angle: Int) : Command {
 
-    constructor(data: ByteArray) : this(ByteBuffer.wrap(data.copyOf(4).apply { reverse() }).int)
+    constructor(data: ByteArray) : this(ByteBuffer.wrap(data.copyOf(4)).apply { order(ByteOrder.LITTLE_ENDIAN) }.int)
 
     override fun getBytes(): ByteArray {
         return byteArrayOf(TURN_TO_ANGLE_CODE.toByte()).plus(ByteBuffer.allocate(4).putInt(angle).array())
